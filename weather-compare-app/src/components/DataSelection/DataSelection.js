ef44e439e75a1f553a87a5b1781b4bfc979aaf65
Dale Matthews <dem7w2@mst.edu>
2020-02-01 13:15:15 -0600

    Convert dataset select to side-scrolling buttons (#7)
    * Setup fancier data selector
    
    * make it pretty

--- weather-compare-app/src/components/DataSelection/DataSelection.js
+++ weather-compare-app/src/components/DataSelection/DataSelection.js
@@ -2,18 +2,30 @@ import React from "react";
 import { connect } from "react-redux";
 import WeatherData from "../../data/weather-data.json";
 import { setDatasetSelection } from "../../redux/actions";
+import "./DataSelection.css";
 
-const DataSelection = ({ setDatasetSelection }) => {
-  const cities = Object.keys(WeatherData)
-  const datasets = Object.keys(WeatherData[cities[0]]).map(dataset => <option>{dataset}</option>);
+
+const DataSelection = ({ selectedDataset, setDatasetSelection }) => {
+  const cities = Object.keys(WeatherData);
+
+  const createClassName = dataset => (dataset === selectedDataset ? 'dataset dataset--selected' : 'dataset');
+
+  const datasets = Object.keys(WeatherData[cities[0]])
+    .map(dataset =>
+      <button key={dataset} className={createClassName(dataset)} onClick={() => setDatasetSelection(dataset)}>
+        {dataset}
+      </button>
+    );
 
   return (
-    <div>
-      <select onChange={e => setDatasetSelection(e.target.value)}>
+    <div className="data-selection-wrapper">
+      <div className="data-selection">
         {datasets}
-      </select>
+      </div>
     </div>
   );
 };
 
-export default connect(null, { setDatasetSelection })(DataSelection);
+const mapStateToProps = ({ selectedDataset }) => ({ selectedDataset });
+
+export default connect(mapStateToProps, { setDatasetSelection })(DataSelection);
