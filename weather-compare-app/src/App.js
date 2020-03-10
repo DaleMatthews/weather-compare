1526b371fa6409a0c1a1dfaa9ccc2e90645ac5ea
DaleMatthews <dem7w2@mst.edu>
2020-03-09 22:22:42 -0500

    Add header

--- weather-compare-app/src/App.js
+++ weather-compare-app/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import DisplayCities from "./components/DisplayCities/DisplayCities";
 import DataSelection from "./components/DataSelection/DataSelection";
+import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import WeatherChart from "./components/WeatherChart/WeatherChart";
 
@@ -9,6 +10,7 @@ class App extends Component {
   render() {
     return (
       <div id="app">
+        <Header />
         <DisplayCities />
         <WeatherChart />
         <DataSelection />
